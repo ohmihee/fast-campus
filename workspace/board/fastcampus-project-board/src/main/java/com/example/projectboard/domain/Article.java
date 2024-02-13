f9015e1dba29820357d7375df87ba11ce73c595e
miheeOh <algml9603@gmail.com>
2024-02-13 19:49:01 +0900

    :feature: article, articleComment 도메인

--- workspace/board/fastcampus-project-board/src/main/java/com/example/projectboard/domain/Article.java
+++ workspace/board/fastcampus-project-board/src/main/java/com/example/projectboard/domain/Article.java
@@ -1,18 +1,83 @@
 package com.example.projectboard.domain;
 
-import java.time.LocalDateTime;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "title"),
+        @Index(columnList = "hashtag"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
+@Entity
 public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Setter
+    @Column(nullable = false)
     private String title; // 제목
+    @Setter
+    @Column(nullable = false, length = 10000)
     private String content; // 본문
+    @Setter
     private String hashtag; // 해시태그
 
 
+    @CreatedDate
+    @Column(nullable = false)
     private LocalDateTime createdAt; // 생성일시
+    @CreatedBy
+    @Column(nullable = false, length = 100)
+    // 누가 등록하였는지에 대한 설정은 config/JpaConfig 클래스에서 설정하여 준다.
     private String createdBy; // 생성자
+    @LastModifiedDate
+    @Column(nullable = false)
     private LocalDateTime modifiedAt; // 수정일시
+    @LastModifiedBy
+    @Column(nullable = false, length = 100)
     private String modifiedBy; // 수정자
+
+
+    // 접근제어자를 protected로 하여 외부에서 New 로 생성하지 못하도록 막음
+    protected Article() {
+    }
+
+    private Article(String title, String content, String hashtag) {
+        this.title = title;
+        this.content = content;
+        this.hashtag = hashtag;
+    }
+    public static Article of(String title, String content, String hashtag) {
+        return new Article(title, content, hashtag);
+    }
+
+    // 엔티티간 동등성 검사 ()
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        //if (o == null || getClass() != o.getClass()) return false;
+        //Article article = (Article) o;
+        //return Objects.equals(id, article.id);
+        if (!(o instanceof Article article)) return false;
+        return id != null && id.equals(article.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 
 
 }
