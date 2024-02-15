8818321569e8e4f3708be9074f41a43b8e2b1617
miheeOh <algml9603@gmail.com>
2024-02-15 20:25:18 +0900

    :feature: data.sql 파일 추가

---
+++ workspace/board/fastcampus-project-board/src/main/resources/data.sql
@@ -0,0 +1,17 @@
+insert into article (
+                     title,
+                     content,
+                     hashtag,
+                     created_by,
+                     modified_by,
+                     created_at,
+                     modified_at )
+values (
+                              'title',
+                              'content',
+                              'hashtag',
+                              'mihee',
+                              'mihh',
+                              '2021-02-02',
+                              '2021-02-02'
+                           );
