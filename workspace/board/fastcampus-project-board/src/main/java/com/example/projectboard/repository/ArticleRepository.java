b1fe50c97850387f85ab5ae6bfd4a0fcbbaa5e19
miheeOh <algml9603@gmail.com>
2024-05-15 13:27:13 +0900

    :fix: data rest test 코드 추가 및 application.yml 설정 변경

--- workspace/board/fastcampus-project-board/src/main/java/com/example/projectboard/repository/ArticleRepository.java
+++ workspace/board/fastcampus-project-board/src/main/java/com/example/projectboard/repository/ArticleRepository.java
@@ -2,6 +2,9 @@ package com.example.projectboard.repository;
 
 import com.example.projectboard.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource// 해당 repository를 기준으로 rest api를 사용할 수 있도록 해주는 어노테이션
 public interface ArticleRepository extends JpaRepository<Article, Long> {
+
 }
