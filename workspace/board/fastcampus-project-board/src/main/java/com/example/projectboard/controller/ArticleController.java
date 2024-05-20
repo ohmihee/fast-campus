786cba30fdf502c24bd55af87818ae41bd345e78
miheeOh <algml9603@gmail.com>
2024-05-20 18:57:10 +0900

    #10 - 게시판 뷰 기본골격 구현

--- workspace/board/fastcampus-project-board/src/main/java/com/example/projectboard/controller/ArticleController.java
+++ workspace/board/fastcampus-project-board/src/main/java/com/example/projectboard/controller/ArticleController.java
@@ -1,8 +1,12 @@
 package com.example.projectboard.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * /articles  - done
@@ -13,4 +17,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/articles")
 @Controller
 public class ArticleController {
+
+    @GetMapping
+    public String articles (ModelMap map) {
+        map.addAttribute("articles", List.of());
+        return "articles/index";
+    }
 }
